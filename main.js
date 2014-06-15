1a1cb8f3a2352033c0dfdf205552cc5daa9a2d0d
matt <mgrichardson@gmail.com>
2014-06-16 11:51:03 +1200

    minor fixes

--- main.js
+++ main.js
@@ -22,11 +22,28 @@ for (i = 0; i<config.usernames.length; i++){
 			util.log(this.username +': Size of the last received tweets : '+ lastTweets.length);
 			this.log(lastTweets);
 		});
+
+		loggers[i].on('endOfTweets', function(logger){
+			var index = loggers.indexOf(logger);
+			if (index > -1) {
+    			loggers.splice(index, 1);
+			}
+			util.log('Logged all tweets for ' + logger.username);
+			util.log('Number of tweets logged for '+logger.username+': '+logger.tweetsLogged);
+			logger.closeDb();
+		})
 	}
 
-	setInterval(function(logger){
-		if(logger.maxIdInit){
-			logger.updateTweets();
+	timerId = setInterval(function(loggers){
+		if (loggers.length === 0){
+			clearInterval(timerId);
 		}
-	}, 10000, loggers[i]);
+		for (j = 0; j < loggers.length; j++){
+			if(loggers[j].maxIdInit){
+				loggers[j].updateTweets();
+				loggers[j].checkRateLimit();
+			}
+		}
+
+	}, 10000, loggers);
 }
